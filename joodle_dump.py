2caf9e7ffe2165a397b4a739e80141a43738fd00
Unnikrishnan Menon <menon.uk1998@gmail.com>
2021-03-31 00:21:47 +0530

    Add files via upload

---
+++ joodle_dump.py
@@ -0,0 +1,115 @@
+import pymunk
+import pygame
+import numpy as np 
+
+class Environment():
+    def __init__(self,space,screen):
+        self.player = Player(100,20,space,screen)
+        self.ground = Ground(1,space,screen)
+        self.walls = Walls(1,space,screen)
+        self.platforms = []
+        self.isplatform = False
+        self.done = False
+        self.space = space
+        self.screen = screen
+
+    def reset(self):
+        self.space.remove(self.rock.circle_shape,self.rock.circle_body)
+        self.space.remove(self.player.triangle_shape,self.player.triangle_body)
+        self.player = Player(100,20,self.space,self.screen)
+        self.done = False
+
+    def add_platform(self,mass,x1,y1,x2,y2):
+        if not self.isplatform:
+            self.platforms.append(Platform(mass,x1,y1,x2,y2,self.space,self.screen))
+
+    def draw_env(self):
+        self.player.draw_player()
+        self.ground.draw_ground()
+        self.walls.draw_walls()
+        for platform in self.platforms:
+            platform.draw_platform()
+        
+    def step(self):
+        self.draw_env()
+        
+class Platform():
+    def __init__(self,mass,x1,y1,x2,y2,space,screen):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        
+        self.mass = mass
+        self.screen = screen
+        self.segment_moment = pymunk.moment_for_segment(self.mass,(self.x1,self.y1),(self.x2,self.y2),2) #end points and thickness
+        self.segment_body = pymunk.Body(self.mass,self.segment_moment,pymunk.Body.STATIC)
+        self.segment_body.position = 0,0
+        self.segment_shape = pymunk.Segment(self.segment_body,(self.x1,self.y1),(self.x2,self.y2),2)
+        self.segment_shape.elasticity = 1
+        self.segment_shape.id = 9
+
+        space.add(self.segment_body,self.segment_shape)
+
+    def draw_platform(self):
+        pygame.draw.line(self.screen, (0,0,200), (self.x1,self.y1),(self.x2,self.y2),2)
+
+class Player():
+    def __init__(self,mass,radius,space,screen):
+        self.mass = mass
+        self.radius  = radius
+        self.elasticity = 1.0
+        self.screen = screen
+        self.circle_moment = pymunk.moment_for_circle(self.mass,0,self.radius)
+        self.circle_body = pymunk.Body(self.mass,self.circle_moment)
+        self.circle_body.position = 0,10
+        self.circle_shape = pymunk.Circle(self.circle_body,self.radius)
+        self.circle_shape.body.velocity = (100,0)
+        self.circle_shape.elasticity = self.elasticity
+        self.circle_shape.id = 2
+
+        space.add(self.circle_body, self.circle_shape)
+        
+    def draw_player(self):
+        pos_x = int(self.circle_body.position.x)
+        pos_y = int(self.circle_body.position.y)
+        pygame.draw.circle(self.screen,(0,0,0),(pos_x,pos_y),30)
+
+class Ground():
+    def __init__(self,mass,space,screen):
+        self.mass = mass
+        self.screen = screen
+        self.segment_moment = pymunk.moment_for_segment(self.mass,(0,750),(1000,750),2) #end points and thickness
+        self.segment_body = pymunk.Body(self.mass,self.segment_moment,pymunk.Body.STATIC)
+        self.segment_body.position = 0,0
+        self.segment_shape = pymunk.Segment(self.segment_body,(0,750),(1000,750),2)
+        self.segment_shape.elasticity = 1
+        self.segment_shape.id = 4
+
+        space.add(self.segment_body,self.segment_shape)
+
+    def draw_ground(self):
+        pygame.draw.rect(self.screen, (0,0,200), pygame.Rect(0, 750, 1000, 0),2)
+
+class Walls():
+    def __init__(self,mass,space,screen):
+        self.mass = mass
+        self.screen = screen
+        self.segment1_moment = pymunk.moment_for_segment(mass,(0,0),(0,800),2) #end points and thickness
+        self.segment1_body = pymunk.Body(self.mass,self.segment1_moment,pymunk.Body.STATIC)
+        self.segment1_body.position = 0,0
+        self.segment1_shape = pymunk.Segment(self.segment1_body,(0,0),(0,800),2)
+        self.segment1_shape.elasticity = 1
+        self.segment2_moment = pymunk.moment_for_segment(self.mass,(1000,0),(1000,800),2) #end points and thickness
+        self.segment2_body = pymunk.Body(self.mass,self.segment2_moment,pymunk.Body.STATIC)
+        self.segment2_body.position = 0,0
+        self.segment2_shape = pymunk.Segment(self.segment2_body,(1000,0),(1000,800),2)
+        self.segment2_shape.elasticity = 1
+        self.segment1_shape.id = 5
+        self.segment2_shape.id = 6
+
+        space.add(self.segment1_body,self.segment1_shape,self.segment2_body,self.segment2_shape)
+
+    def draw_walls(self):
+        pygame.draw.rect(self.screen, (0,200,0), pygame.Rect(0, 0, 0, 800),2)
+        pygame.draw.rect(self.screen, (0,200,0), pygame.Rect(1000, 0, 0, 800),2)
---
+++ main.py
@@ -0,0 +1,48 @@
+import pymunk
+import pygame
+from joodle_dump import Ground, Player, Walls, Platform, Environment
+
+pygame.init()
+screen = pygame.display.set_mode((1000,800))
+clock = pygame.time.Clock()
+space = pymunk.Space()
+space.gravity = (0,100)
+
+env = Environment(space,screen)
+
+done = False
+
+def coll_post(arbiter,space,data):
+    if arbiter.shapes[1].id == 9 and arbiter.shapes[0].id == 2: 
+        space.remove(arbiter.shapes[1].body,arbiter.shapes[1])
+        env.platforms.pop()
+        env.isplatform = False
+    
+    if arbiter.shapes[0].id == 9 and arbiter.shapes[1].id == 2: 
+        space.remove(arbiter.shapes[0].body,arbiter.shapes[0])
+        env.platforms.pop()
+        env.isplatform = False
+
+
+handler = space.add_default_collision_handler()
+handler.post_solve = coll_post
+
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x1, y1 = pygame.mouse.get_pos() 
+        if event.type == pygame.MOUSEBUTTONUP:
+            x2, y2 = pygame.mouse.get_pos() 
+            env.add_platform(1,x1,y1,x2,y2)
+            env.isplatform = True
+
+
+    screen.fill((50,50,50))
+    env.step()
+    space.step(1/50)
+    pygame.display.update()
+    clock.tick(120)
